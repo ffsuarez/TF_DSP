9e44a80970a90f31d58d683fad086d8ee30c4a1d
ffsuarez <fmsuarez92@gmail.com>
2018-12-28 10:57:24 -0300

    se definen comentarios de parametros a usar

--- LK.py
+++ LK.py
@@ -1,3 +1,13 @@
+'''
+Como se pretende que sea el uso:
+
+LK.py [--tecnica<=lk|shi-tom] [<fuente_video>]
+
+Donde: --tecnica decide cual tecnica tomar, Lucas Kanade o Shi-Tomasi
+       [<fuente_video>] elige un archivo de video y lo lee, sino toma la camara
+'''
+
+
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,5 +19,6 @@ from common import anorm, getsize
 if __name__=='__main__':
 	print(__doc__)
 	import sys,getopt
-	opcs,args=getopt.getopt   #http://pyspanishdoc.sourceforge.net/lib/module-getopt.html
+	#opcs,args=getopt.getopt   #http://pyspanishdoc.sourceforge.net/lib/module-getopt.html
+	
 	
