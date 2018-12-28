bddb6efca0f7a3f244a421484935b6b36f18b3dd
ffsuarez <fmsuarez92@gmail.com>
2018-12-28 10:42:06 -0300

    inicial

---
+++ LK.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+import os
+
+from common import anorm, getsize
+
+if __name__=='__main__':
+	print(__doc__)
+	import sys,getopt
+	opcs,args=getopt.getopt   #http://pyspanishdoc.sourceforge.net/lib/module-getopt.html
+	
