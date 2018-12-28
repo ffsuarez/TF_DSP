1bb36e204231330f16104864ba1c86ddde7be64a
ffsuarez <fmsuarez92@gmail.com>
2018-12-28 19:31:17 -0300

    correcciones varias

--- LK.py
+++ LK.py
@@ -7,31 +7,30 @@ Donde: [--tecnica] decide cual tecnica tomar, Lucas Kanade o Shi-Tomasi
        [<fuente_video>] elige un archivo de video y lo lee, sino toma la camara
 	   [n_objetos] es el numero de objetos a seguir
 '''
-
+#https://www.digitalocean.com/community/tutorials/how-to-use-the-python-debugger
 
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import pdb
 #---------------------------------------------------------------------
-def selector_rois(n):
-	for i in range(n):
-		#https://www.learnopencv.com/how-to-select-a-bounding-box-roi-in-opencv-cpp-python/
-		r=cv.selectROI(frame)
-		#seleccionar imagen
-		Imcrop[i]=frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
-		cv.waitKey(0)
+def selector_rois(n,cap):
+        _ret,frame=cap.read()
+        Imcrop=[]
+        for i in range(n):
+
+            #https://www.learnopencv.com/how-to-select-a-bounding-box-roi-in-opencv-cpp-python/
+            r=cv.selectROI(frame)
+            pdb.set_trace()
+            #seleccionar imagen
+
+            Imcrop.append(frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])])
+            cv.waitKey(0)
 
 
 
 #---------------------------------------------------------------------
 class seguidor:
-	def __init__(self,video_src):
-		try:
-			cap=cv.videoCapture(video_src)
-		except:
-			cap=cv.videoCapture(0)
-	
-	
 	def opciones(self,metod):
 		#https://stackoverflow.com/questions/4117530/sys-argv1-meaning-in-script
 		#https://pymotw.com/2/getopt/
@@ -53,15 +52,19 @@ class seguidor:
 			
 	
 	
-	#def seguimiento(self,Imcrop):
+	def seguimiento(self,Imcrop):
 		
 	
 
 	def run (self):
-		while(True):
-			_ret,frame=cap.read()
-			selector_rois(n)
-			#seguidor.seguimiento(None,Imcrop)
+            try:
+                cap=cv.VideoCapture(video_src)                            
+            except:
+                cap=cv.VideoCapture(0)                           
+
+            while(True):
+                selector_rois(n,cap)
+                seguidor.seguimiento(None,Imcrop)
 		
 
 
@@ -74,14 +77,16 @@ if __name__=='__main__':
 	print(__doc__)
 	import sys,getopt
 	#opcs,args=getopt.getopt   #http://pyspanishdoc.sourceforge.net/lib/module-getopt.html
-	
-	metodo=sys.argv[1]
-	video_src=sys.argv[2]
-	n=sys.argv[3]
-	n=int(n)
+##	metodo=sys.argv[1]
+##	video_src=sys.argv[2]
+##	n=sys.argv[3]
+##	n=int(n)
+	metodo='lk'
+	video_src=0
+	n=1
 	#https://stackoverflow.com/questions/2709821/what-is-the-purpose-of-self
-	#https://es.stackoverflow.com/questions/202588/como-funciona-self-en-python
-	seguidor.__init__(None,video_src)
+	#https://es.stackoverflow.com/questions/202588/como-funciona-self-en-python	
+	seguidor.__init__(None,video_src)    
 	seguidor.opciones(None,metodo)
-	seguidor.run()
+	seguidor.run(None)
 	
